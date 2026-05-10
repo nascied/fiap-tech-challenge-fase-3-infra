cbea5d78c197087a4fe4272233eec3813b040ae7
edson.nascimento <edsonleandro21@gmail.com>
2026-05-10 12:49:12 -0300

    fix: removido configuração de backend remote

--- iac/terraform/bootstrap-backend/provider.tf
+++ iac/terraform/bootstrap-backend/provider.tf
@@ -1,9 +1,11 @@
-terraform {
-  required_version = ">= 1.10.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "6.44.0"
+
+
+provider "aws" {
+  region = "us-east-1"
+  
+  default_tags {
+    tags = {
+      created_by = "terraform"
     }
   }
 }
--- iac/terraform/bootstrap-backend/required.tf
+++ iac/terraform/bootstrap-backend/required.tf
@@ -1,8 +1,9 @@
-provider "aws" {
-
-  default_tags {
-    tags = {
-      created_by = "terraform"
+terraform {
+  required_version = ">= 1.10.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.44.0"
     }
   }
 }
